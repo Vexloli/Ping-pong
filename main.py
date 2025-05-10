c1f92afe3b626279ad344ff1ca2dc55f4f34a953
Vexloli <fap.fap.95@bk.ru>
2025-05-10 17:16:04 +0300

    Update main.py
    v1.1

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QLabel, QVBoxLayout
 
+
+#bfdgbdjhfbvlkdf;knvbdfkljgdfv
+#bkgdfolbhbfd
+
+
 app = QApplication([])
 main_win = QWidget()
 main_win.show()
